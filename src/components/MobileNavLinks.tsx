a5188e377b96acc308a06c09947321db12c4cb32
Gabriel Boldea <gabrielboldea@192.168.1.27>
2024-04-24 05:34:59 +0300

    add restaurantpage

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -12,6 +12,12 @@ const MobileNAvLinks = () => {
             > 
                 Profile 
             </Link>
+            <Link 
+                to="/manage-restaurant" 
+                className="flex bg-white items-center font-bold hover:text-orange-500"
+            >
+                        Manage Restaurant
+            </Link>
             <Button 
                 onClick={() => logout()}
                 className="flex items-center px-3 font-bold hover:bg-gray-500">
